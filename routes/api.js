25c025ca03d6f40be2d1de9697255c9cb5d2a450
Jack <jacksdlr@gmail.com>
2023-02-22 16:34:18 +0000

    feat: user schema

--- routes/api.js
+++ routes/api.js
@@ -28,9 +28,16 @@ module.exports = (app) => {
         exercises: [exerciseSchema]
     }, {versionKey: false})
 
+    const userSchema = new Schema({
+        username: String,
+        password: String,
+        workouts: [workoutSchema]
+    }, {versionKey: false})
+
     const Set = mongoose.model("Set", setSchema)
     const Exercise = mongoose.model("Exercise", exerciseSchema)
     const Workout = mongoose.model("Workout", workoutSchema)
+    const User = mongoose.model("User", userSchema)
 
     app.route("/api/exercises/:workout")
         .get((req, res) => {
