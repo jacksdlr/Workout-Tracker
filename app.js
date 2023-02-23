81d79718d2cd1d029f92b9462583464e6022db9f
Jack <jacksdlr@gmail.com>
2023-02-23 23:06:25 +0000

    feat: signup routing, creates user in db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const pug = require("pug")
 const bodyParser = require("body-parser")
+const bcrypt = require("bcrypt")
 
 require("dotenv").config();
 
@@ -15,6 +16,7 @@ const app = express();
 // Middleware
 app.set("view engine", "pug")
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 app.use("/", bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use("/public", express.static(process.cwd()+ "/public"))
@@ -25,12 +27,42 @@ app.route("/")
         res.render("index")
     })
 
-app.get("/login", (req, res) => {
-    res.render("login-signup")
-})
-app.get("/signup", (req, res) => {
-    res.render("login-signup")
-})
+app.route("/login")
+    .get((req, res) => {
+        res.render("login-signup")
+    })
+    .post((req, res) => {
+
+    })
+
+app.route("/signup")
+    .get((req, res) => {
+        res.render("login-signup")
+    })
+    .post(async (req, res) => {
+        try {
+            const {username, password} = req.body
+            const hashedPassword = await bcrypt.hash(password, 10)
+            User.findOne({username}, (err, existingUser) => {
+                if (existingUser) {
+                    res.render("/login-signup", {signupError: "User already exists"})
+                } else {
+                    User.create({
+                        username,
+                        password: hashedPassword
+                    }, (err, newUser) => {
+                        if (err) {
+                            res.status(400).send(err)
+                        } else {
+                            res.redirect("/") // MIGHT NEED TO CHANGE THIS 13:45 IN VIDEO
+                        }
+                    })
+                }
+            })
+        } catch {
+            res.redirect("/signup")
+        }
+    })
 
 const port = 3000
 
