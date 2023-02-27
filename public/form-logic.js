7e7a16affb14eb74b1866630102fb4f483528443
Jack <jacksdlr@gmail.com>
2023-02-27 13:07:30 +0000

    fix: required exercise name if superset box is checked

--- public/form-logic.js
+++ public/form-logic.js
@@ -1,17 +1,20 @@
 const supersetCheckDiv = document.querySelector("#superset-check")
 const supersetCheckbox = document.querySelector("#superset-input")
 const supersetInputContainers = document.querySelectorAll(".superset-input-container")
+const supersetExercise = document.getElementById("superset-exercise-input")
 supersetCheckbox.addEventListener("change", () => {
     if (supersetCheckbox.checked) {
         supersetInputContainers.forEach((container) => {
             container.style.visibility = "visible"
             container.style.display = "block"
         })
+        supersetExercise.setAttribute("required", "true")
     } else {
         supersetInputContainers.forEach((container) => {
             container.style.visibility = "hidden"
             container.style.display = "none"
         })
+        supersetExercise.setAttribute("required", "false")
     }
 })
 /*
