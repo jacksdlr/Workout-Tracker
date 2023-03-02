a3e129ff7c598ef906c5d07a660ce85d664dac2d
Jack <jacksdlr@gmail.com>
2023-03-02 18:21:45 +0000

    feat: renders workout for current date on load, created functions for rendering exercise and set loadouts to be called where needed

--- public/javascripts/workout-render.js
+++ public/javascripts/workout-render.js
@@ -1,51 +1,69 @@
+const xhttp = new XMLHttpRequest()
+
 const form = document.getElementById("exercise-form")
+const workoutContainer = document.querySelector(".workout-container")
+
+xhttp.open("GET", "/workouts/" + new Date().toISOString().split("T")[0])
+xhttp.send()
+xhttp.onload = function () {
+    console.log(JSON.stringify(JSON.parse(this.response), null, 4))
+    renderWorkout(JSON.parse(this.response))
+}
+
 form.addEventListener("submit", () => {
     setTimeout(() => {
         // Get the date and submit a GET request to return the user's workout for that date
         const date = document.getElementById("workout-input").value
-        const xhttp = new XMLHttpRequest()
+        console.log(date)
+        
         xhttp.open("GET", "/workouts/" + date)
         xhttp.send()
         xhttp.onload = function () {
             // Clear the comment fields
             resetComments()
-            
+
             // Parse the response data
-            let data = JSON.parse(this.response)
-            console.log(JSON.stringify(data, null, 4))
-            renderWorkout(data)
+            console.log(JSON.stringify(JSON.parse(this.response), null, 4))
+            renderWorkout(JSON.parse(this.response))
         }
     }, 250);
 
 })
 
 const renderWorkout = (data) => {
-    const workoutContainer = document.querySelector(".workout-container")
     data.forEach(exercise => {
         let exerciseContainer = document.getElementById(exercise._id)
         console.log(exerciseContainer)
         console.log("Test")
         if (!exerciseContainer) {
             console.log("no exercise container found")
-            exerciseContainer = document.createElement("div")
-            exerciseContainer.classList.add("exercise-container")
-            exerciseContainer.setAttribute("id", exercise._id)
-            workoutContainer.appendChild(exerciseContainer)
+            createExerciseContainer(exercise)
+            createSetContainers(exercise)
         } else {
             console.log("found exercise container")
-            exercise.sets.forEach(set => {
-                let setContainer = document.getElementById(set._id)
-                if (!setContainer) {
-                    console.log("no set container found")
-                    setContainer = document.createElement("div")
-                    setContainer.classList.add("set-container")
-                    setContainer.setAttribute("id", set._id)
-                    exerciseContainer.appendChild(setContainer)
-                } else {
-                    console.log("found set container")
-                }
-            })
-            
+            createSetContainers(exercise)
+        }
+    })
+}
+
+const createExerciseContainer = (exercise) => {
+    exerciseContainer = document.createElement("div")
+    exerciseContainer.classList.add("exercise-container")
+    exerciseContainer.setAttribute("id", exercise._id)
+    workoutContainer.appendChild(exerciseContainer)
+}
+
+const createSetContainers = (exercise) => {
+    exercise.sets.forEach(set => {
+        let setContainer = document.getElementById(set._id)
+        if (!setContainer) {
+            console.log("no set container found")
+            setContainer = document.createElement("div")
+            setContainer.classList.add("set-container")
+            setContainer.setAttribute("id", set._id)
+            exerciseContainer.appendChild(setContainer)
+        } else {
+            console.log("found set container")
         }
     })
 }
