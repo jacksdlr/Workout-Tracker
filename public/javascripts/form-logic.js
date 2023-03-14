ec9c5294099783a395528575fb882da9f7db6ad5
Jack <jacksdlr@gmail.com>
2023-03-14 16:52:14 +0000

    fix: superset containers display to flex rather than block

--- public/javascripts/form-logic.js
+++ public/javascripts/form-logic.js
@@ -20,7 +20,7 @@ const toggleVisibility = () => {
     if (supersetCheckbox.checked) {
         supersetInputContainers.forEach((container) => {
             container.style.visibility = "visible"
-            container.style.display = "block"
+            container.style.display = "flex"
         })
         supersetExercise.setAttribute("required", "true")
         // If it is unselected it resets the inputs to again be hidden, and exercise name is no longer required to submit the form
