dfe1c2b89a4bbb47b70eee4d17f459914ccdd92e
Jack <jacksdlr@gmail.com>
2023-03-24 16:56:50 +0000

    feat: moved editing on right click to seperate javascript file, right clicking exercise name now brings up custom context menu at mouse position and adds the respective event listener for clicking edit exercise name

--- public/javascripts/workout-render.js
+++ public/javascripts/workout-render.js
@@ -5,6 +5,12 @@ const workoutContainer = document.querySelector(".workout-exercises")
 const workoutTitle = document.getElementById("workout-name")
 const displayDate = document.getElementById("display-date")
 
+const exerciseMenu = document.getElementById("exercise-menu")
+const weightMenu = document.getElementById("weight-menu")
+const setMenu = document.getElementById("set-menu")
+const supersetMenu = document.getElementById("superset-menu")
+const supersetRepsMenu = document.getElementById("superset-reps-menu")
+
 if (inputDate.value == "") {
     inputDate.value = new Date().toISOString().split("T")[0]
 }
@@ -118,20 +124,17 @@ const createExerciseContainer = (exercise) => {
     // Right click opens a prompt to change the exercise name
     exerciseName.addEventListener("contextmenu", (e) => {
         e.preventDefault()
-        let newName = prompt("New exercise name: ", exercise.exercise_name)
 
-        if (newName != null && !newName.match(/^\s+$/) && newName != "" && newName != exercise.exercise_name) {
-            xhttp.open("POST", "/update/exercise")
-            xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8")
-            xhttp.send(JSON.stringify({ oldName: exerciseName.textContent, newName, date: displayDate.value }))
-            sessionStorage.exercise_name = newName
-            xhttp.onload(() => {
-                console.log("im here")
-                renderWorkout(JSON.parse(this.response), true, displayDate.value)
-                toggleRequired()
-                populate()
-            })
-        }
+        // Custom right click menu to edit or delete exercise
+        const {clientX, clientY} = e
+        console.log(exerciseMenu)
+        exerciseMenu.style.top = `${clientY}px`
+        exerciseMenu.style.left = `${clientX}px`
+        exerciseMenu.style.visibility = "visible"
+
+        editExerciseName(exercise.exercise_name, displayDate.value)
+
+        
     })
     exerciseContainer.appendChild(exerciseName)
 
@@ -148,18 +151,7 @@ const addExerciseComments = (exercise) => {
             exerciseComment.textContent = comment
             exerciseComment.addEventListener("contextmenu", (e) => {
                 e.preventDefault()
-                let editedComment = prompt("New comment: ", comment)
-                if (editedComment != null && !editedComment.match(/^\s+$/) && editedComment != "" && editedComment != comment) {
-                    xhttp.open("POST", "/update/exercise_comments")
-                    xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8")
-                    xhttp.send(JSON.stringify({ exercise_name: exercise.exercise_name, commentIndex: index, editedComment, date: displayDate.value }))
-                    sessionStorage.exercise_name = exercise.exercise_name
-                    xhttp.onload(() => {
-                        renderWorkout(JSON.parse(this.response), true, displayDate.value)
-                        toggleRequired()
-                        populate()
-                    })
-                }
+                
 
             })
             exerciseComments.appendChild(exerciseComment)
@@ -210,29 +202,7 @@ const populateweightContainers = (exercise, set, weightContainer) => {
     setWeight.textContent = set_weight
     setWeightAndCount.addEventListener("contextmenu", (e) => {
         e.preventDefault()
-        let newWeight = prompt("New weight used: ", set_weight)
-
-        if (newWeight != null && !newWeight.match(/^\s+$/) && newWeight != set_weight) {
-            if (!newWeight.match(/(^(\d+\.\d{0,2}|\d+)(kg|lbs)$)|(^$)/)) {
-                console.log(newWeight)
-                alert("Make sure your new weight follows the structure [x]kg/lbs or [x.xx]kg/lbs.")
-                return
-            } else {
-                xhttp.open("POST", "/update/weight")
-                xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8")
-                xhttp.send(JSON.stringify({ exercise_name: exercise.exercise_name, set_id: set._id, newWeight, date: displayDate.value }))
-                sessionStorage.exercise_name = exercise.exercise_name
-                /*
-                sessionStorage.set_weight = newWeight.split(/(\d+\.\d{0,2}|\d+)/)[1]
-                sessionStorage.set_weight_unit = newWeight.split(/(\d+\.\d{0,2}|\d+)/)[2]
-                */
-                xhttp.onload(() => {
-                    renderWorkout(JSON.parse(this.response), true, displayDate.value)
-                    toggleRequired()
-                    populate()
-                })
-            }
-        }
+        
     })
     setWeightAndCount.appendChild(setWeight)
 
@@ -266,29 +236,7 @@ const populateweightContainers = (exercise, set, weightContainer) => {
             }
             setRep.addEventListener("contextmenu", (e) => {
                 e.preventDefault()
-                let newReps = prompt(`New reps count for set ${index+1}: `, reps)
-
-                if (newReps != null && !newReps.match(/^\s+$/) && newReps != "" && newReps != reps) {
-                    if (!newReps.match(/^\d+$/)) {
-                        alert("Please enter a valid number for reps performed.")
-                        return
-                    } else {
-                        xhttp.open("POST", "/update/reps")
-                        xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8")
-                        xhttp.send(JSON.stringify({ exercise_name: exercise.exercise_name, set_id: set._id, repsIndex: index, newReps, date: displayDate.value }))
-                        sessionStorage.exercise_name = exercise.exercise_name
-                        /*
-                        sessionStorage.set_weight = set.set_weight.split(/(\d+\.\d{0,2}|\d+)/)[1]
-                        sessionStorage.set_weight_unit = set.set_weight.split(/(\d+\.\d{0,2}|\d+)/)[2]
-                        sessionStorage.set_reps = newReps
-                        */
-                        xhttp.onload(() => {
-                            renderWorkout(JSON.parse(this.response), true, displayDate.value)
-                            toggleRequired()
-                            populate()
-                        })
-                    }
-                }
+                
             })
             setReps.append(setRep)
         })
@@ -313,25 +261,7 @@ const populateweightContainers = (exercise, set, weightContainer) => {
         supersetExercise.textContent = superset_exercise
         supersetExercise.addEventListener("contextmenu", (e) => {
             e.preventDefault()
-            let newName = prompt("New superset exercise name: ", superset_exercise)
-
-            if (newName != null && !newName.match(/^\s+$/) && newName != "" && newName != superset_exercise) {
-                xhttp.open("POST", "/update/superset_name")
-                xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8")
-                xhttp.send(JSON.stringify({ exercise_name: exercise.exercise_name, set_id: set._id, newName, date: displayDate.value }))
-                sessionStorage.exercise_name = exercise.exercise_name
-                /*
-                sessionStorage.set_weight = set.set_weight.split(/(\d+\.\d{0,2}|\d+)/)[1]
-                sessionStorage.set_weight_unit = set.set_weight.split(/(\d+\.\d{0,2}|\d+)/)[2]
-                sessionStorage.superset = true
-                sessionStorage.superset_exercise = newName
-                */
-                xhttp.onload(() => {
-                    renderWorkout(JSON.parse(this.response), true, displayDate.value)
-                    toggleRequired()
-                    populate()
-                })
-            }
+            
         })
         supersetExerciseAndWeight.appendChild(supersetExercise)
 
@@ -340,31 +270,7 @@ const populateweightContainers = (exercise, set, weightContainer) => {
         supersetWeight.textContent = superset_weight
         supersetWeight.addEventListener("contextmenu", (e) => {
             e.preventDefault()
-            let newWeight = prompt("New superset weight used: ", superset_weight)
-    
-            if (newWeight != null && !newWeight.match(/^\s+$/) && newWeight != superset_weight) {
-                if (!newWeight.match(/(^(\d+\.\d{0,2}|\d+)(kg|lbs)$)|(^$)/)) {
-                    alert("Make sure your new weight follows the structure [x]kg/lbs or [x.xx]kg/lbs.")
-                    return
-                } else {
-                    xhttp.open("POST", "/update/superset_weight")
-                    xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8")
-                    xhttp.send(JSON.stringify({ exercise_name: exercise.exercise_name, set_id: set._id, newWeight, date: displayDate.value }))
-                    sessionStorage.exercise_name = exercise.exercise_name
-                    /*
-                    sessionStorage.set_weight = set.set_weight.split(/(\d+\.\d{0,2}|\d+)/)[1]
-                    sessionStorage.set_weight_unit = set.set_weight.split(/(\d+\.\d{0,2}|\d+)/)[2]
-                    sessionStorage.superset_exercise = set.superset_exercise
-                    sessionStorage.superset_weight = newWeight.split(/(\d+\.\d{0,2}|\d+)/)[1]
-                    sessionStorage.superset_weight_unit = newWeight.split(/(\d+\.\d{0,2}|\d+)/)[2]
-                    */
-                    xhttp.onload(() => {
-                        renderWorkout(JSON.parse(this.response), true, displayDate.value)
-                        toggleRequired()
-                        populate()
-                    })
-                }
-            }
+            
         })
         supersetExerciseAndWeight.appendChild(supersetWeight)
 
@@ -386,29 +292,7 @@ const populateweightContainers = (exercise, set, weightContainer) => {
             }
             supersetRep.addEventListener("contextmenu", (e) => {
                 e.preventDefault()
-                let newReps = prompt(`New superset reps count for set ${index+1}: `, reps)
-
-                if (newReps != null && !newReps.match(/^\s+$/) && newReps != "" && newReps != reps) {
-                    if (!newReps.match(/^\d+$/)) {
-                        alert("Please enter a valid number for reps performed.")
-                        return
-                    } else {
-                        xhttp.open("POST", "/update/superset_reps")
-                        xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8")
-                        xhttp.send(JSON.stringify({ exercise_name: exercise.exercise_name, set_id: set._id, repsIndex: index, newReps, date: displayDate.value }))
-                        sessionStorage.exercise_name = exercise.exercise_name
-                        /*
-                        sessionStorage.set_weight = set.set_weight.split(/(\d+\.\d{0,2}|\d+)/)[1]
-                        sessionStorage.set_weight_unit = set.set_weight.split(/(\d+\.\d{0,2}|\d+)/)[2]
-                        sessionStorage.set_reps = newReps
-                        */
-                        xhttp.onload(() => {
-                            renderWorkout(JSON.parse(this.response), true, displayDate.value)
-                            toggleRequired()
-                            populate()
-                        })
-                    }
-                }
+                
             })
             supersetReps.append(supersetRep)
         })
@@ -426,20 +310,7 @@ const populateweightContainers = (exercise, set, weightContainer) => {
             newComment.textContent = comment
             newComment.addEventListener("contextmenu", (e) => {
                 e.preventDefault()
-                let setIndex = comment.split(": ")[0]
-                let editedComment = prompt(`New comment for set ${setIndex.split(" ")[1]}: `, comment.split(": ")[1])
-                if (editedComment != null && !editedComment.match(/^\s+$/) && editedComment != "" && editedComment != comment) {
-                    editedComment = `${setIndex}: ` + editedComment
-                    xhttp.open("POST", "/update/set_comments")
-                    xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8")
-                    xhttp.send(JSON.stringify({ exercise_name: exercise.exercise_name, set_id: set._id, commentIndex: index, editedComment, date: displayDate.value }))
-                    sessionStorage.exercise_name = exercise.exercise_name
-                    xhttp.onload(() => {
-                        renderWorkout(JSON.parse(this.response), true, displayDate.value)
-                        toggleRequired()
-                        populate()
-                    })
-                }
+                
 
             })
             setComments.appendChild(newComment)
